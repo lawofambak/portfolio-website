662e2b98fc7ea908fc8fe95af7dd62fcfd9f7239
Alex Kim <lawofambak7@gmail.com>
2023-03-09 14:09:11 -0800

    Change text color

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,6 +1,6 @@
-import { useState } from 'react';
-import AnchorLink from 'react-anchor-link-smooth-scroll';
-import useMediaQuery from '../hooks/useMediaQuery';
+import { useState } from "react";
+import AnchorLink from "react-anchor-link-smooth-scroll";
+import useMediaQuery from "../hooks/useMediaQuery";
 
 const Link = ({ page, currentPage, setCurrentPage }) => {
   const pageLowerCased = page.toLowerCase();
@@ -15,98 +15,101 @@ const Link = ({ page, currentPage, setCurrentPage }) => {
       {page}
     </AnchorLink>
   );
-}
+};
 
 const Navbar = ({ topOfPage, currentPage, setCurrentPage }) => {
   const [menuToggled, setMenuToggled] = useState(false);
   const isAboveSmallScreens = useMediaQuery("(min-width: 768px)");
-  const navbarColor = topOfPage? "" : "bg-blue";
+  const navbarColor = topOfPage ? "" : "bg-blue";
 
-  return(
-    <nav className={`${navbarColor} z-40 w-full fixed top-0 py-6`} >
+  return (
+    <nav className={`${navbarColor} z-40 w-full fixed top-0 py-6`}>
       <div className="flex items-center justify-between mx-auto w-5/6">
-      <h4 className="font-playfair text-4xl font-bold">AK</h4>
+        <h4 className="font-playfair text-4xl font-bold">AK</h4>
 
-      {/* DESKTOP VERSION */}
-      {isAboveSmallScreens ? (
-        <div className="flex justify-between gap-16 font-sourcecodepro text-md font-semibold">
-          <Link 
-            page="Home"
-            currentPage={currentPage}
-            setCurrentPage={setCurrentPage}
-          />
-          <Link 
-            page="About"
-            currentPage={currentPage}
-            setCurrentPage={setCurrentPage}
-          />
-          <Link 
-            page="Skills"
-            currentPage={currentPage}
-            setCurrentPage={setCurrentPage}
-          />
-          <Link 
-            page="Projects"
-            currentPage={currentPage}
-            setCurrentPage={setCurrentPage}
-          />
-          <Link 
-            page="Contact"
-            currentPage={currentPage}
-            setCurrentPage={setCurrentPage}
-          />
-        </div>
-      ) : (
-        <div>
-        <button
-          className="rounded-full p-2 text-4xl"
-          onClick={() => setMenuToggled(!menuToggled)}
-        >
-          ⚙︎
-        </button>
-        </div>
-      )}
-
-      {/* MOBILE VERSION */}
-      {!isAboveSmallScreens && menuToggled && (
-        <div className="fixed right-0 bottom-0 h-full bg-blue w-[300px]">
-          <div className="flex justify-end p-10">
-            <button className="text-4xl text-dark-blue" onClick={() => setMenuToggled(!menuToggled)}>
-              ✘
-            </button>
-          </div>
-            <div className="flex flex-col gap-10 ml-[33%] text-2xl text-dark-blue">
+        {/* DESKTOP VERSION */}
+        {isAboveSmallScreens ? (
+          <div className="flex justify-between gap-16 font-sourcecodepro text-md font-semibold">
             <Link
               page="Home"
               currentPage={currentPage}
               setCurrentPage={setCurrentPage}
             />
-            <Link 
+            <Link
               page="About"
               currentPage={currentPage}
               setCurrentPage={setCurrentPage}
             />
-            <Link 
+            <Link
               page="Skills"
               currentPage={currentPage}
               setCurrentPage={setCurrentPage}
             />
-            <Link 
+            <Link
               page="Projects"
               currentPage={currentPage}
               setCurrentPage={setCurrentPage}
             />
-            <Link 
+            <Link
               page="Contact"
               currentPage={currentPage}
               setCurrentPage={setCurrentPage}
             />
           </div>
-        </div>
-      )}
+        ) : (
+          <div>
+            <button
+              className="rounded-full p-2 text-4xl"
+              onClick={() => setMenuToggled(!menuToggled)}
+            >
+              ⚙︎
+            </button>
+          </div>
+        )}
+
+        {/* MOBILE VERSION */}
+        {!isAboveSmallScreens && menuToggled && (
+          <div className="fixed right-0 bottom-0 h-full bg-blue w-[300px]">
+            <div className="flex justify-end p-10">
+              <button
+                className="text-4xl text-dark-blue"
+                onClick={() => setMenuToggled(!menuToggled)}
+              >
+                ✘
+              </button>
+            </div>
+            <div className="flex flex-col gap-10 ml-[33%] text-2xl text-white">
+              <Link
+                page="Home"
+                currentPage={currentPage}
+                setCurrentPage={setCurrentPage}
+              />
+              <Link
+                page="About"
+                currentPage={currentPage}
+                setCurrentPage={setCurrentPage}
+              />
+              <Link
+                page="Skills"
+                currentPage={currentPage}
+                setCurrentPage={setCurrentPage}
+              />
+              <Link
+                page="Projects"
+                currentPage={currentPage}
+                setCurrentPage={setCurrentPage}
+              />
+              <Link
+                page="Contact"
+                currentPage={currentPage}
+                setCurrentPage={setCurrentPage}
+              />
+            </div>
+          </div>
+        )}
       </div>
     </nav>
   );
-}
+};
 
 export default Navbar;
